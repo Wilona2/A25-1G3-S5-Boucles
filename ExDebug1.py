ff7a738dc101a91b4dc0c5ffb4f51a49834b5df9
Wilona2 <2172985@cegepoutaouais.qc.ca>
2025-09-18 11:13:18 -0400

    ??

--- ExDebug1.py
+++ ExDebug1.py
@@ -43,4 +43,9 @@ def environnement_optimal(temp, poussiere, humidite):
 
 
 if __name__ == "__main__":
-    print(environnement_optimal(25, "faible", 40))
+    #print(environnement_optimal(25, "faible", 40))
+    temp = float(input("Entrez la température : "))
+    poussiere = input("Entrez le niveau de poussière (faible, moyen, élevé) : ")
+    humidite = float(input("Entrez l'humidité : "))
+
+    print(environnement_optimal(temp, poussiere, humidite))
